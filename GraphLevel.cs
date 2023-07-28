2f085bfbd8f0fc2aeaed7c2e86a8915bb5faeef6
haxnted <bagnencko.sasha1@gmail.com>
2023-07-28 16:55:02 +0300

    version 1.0. Create Gui and logic typing text

---
+++ GraphLevel.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Windows.Controls;
+
+namespace TrainerTextsWPF
+{
+    public enum NameLevel
+    {
+        Newcomer,
+        Apprentice,
+        Amateur,
+        Advanced,
+        Expert,
+        Master,
+        Virtuoso,
+        Guru,
+        Professional,
+    }
+    public abstract class GraphOperations
+    {
+        protected abstract string GetPath();
+        protected abstract void Serialize();
+        protected abstract object Deserialize();
+        protected abstract void CreateFile();
+    }
+    public sealed class GraphLevel : GraphOperations
+    {
+        private InfoLevel _infoLevel = new InfoLevel();
+        private string PathFile { get; set; }
+        private readonly string _fileName = "level.json";
+        private readonly Dictionary<int, NameLevel> infoLevels = new()
+        {
+            {1000, NameLevel.Newcomer},
+            {5000, NameLevel.Apprentice},
+            {20000, NameLevel.Amateur},
+            {50000, NameLevel.Advanced},
+            {100000, NameLevel.Expert},
+            {200000, NameLevel.Master},
+            {350000, NameLevel.Virtuoso},
+            {500000, NameLevel.Guru},
+            {1000000, NameLevel.Professional}
+        };
+
+        public GraphLevel()
+        {
+            PathFile = GetPath();
+            CreateFile();
+            _infoLevel = (InfoLevel)Deserialize();
+        }
+
+        protected override string GetPath() =>
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "TrainerTexts", _fileName);
+
+        protected override void CreateFile()
+        {
+            if (File.Exists(PathFile))
+                return;
+
+            using (File.Create(PathFile)) { };
+            _infoLevel.CountLetters = 0;
+            _infoLevel.Level = NameLevel.Newcomer;
+            _infoLevel.MaximumLenghtProgressBar = 1000;
+            Serialize();
+        }
+
+        protected override void Serialize()
+        {
+            string result = JsonSerializer.Serialize(_infoLevel);
+            using StreamWriter streamwriter = new StreamWriter(PathFile);
+            streamwriter.Write(result);
+            streamwriter.Close();
+        }
+
+        protected override object Deserialize()
+        {
+            InfoLevel? info = new InfoLevel();
+            string json;
+            using (StreamReader streamReader = new StreamReader(PathFile))
+            {
+                json = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+            if (!string.IsNullOrEmpty(json))
+            {
+                info = JsonSerializer.Deserialize<InfoLevel>(json);
+
+                if (info is null)
+                    throw new Exception($"Error deserialize in class {typeof(InfoLevel)}");
+            }
+            return info;
+        }
+
+        public ProgressBar UpdateGraph(ProgressBar progressBar, ref TextBlock textBox, int countletters = 0)
+        {
+            if (_infoLevel.Level == NameLevel.Professional)
+                return progressBar;
+
+            _infoLevel.CountLetters += countletters;
+            if (_infoLevel.MaximumLenghtProgressBar < _infoLevel.CountLetters)
+            {
+                var enumerator = infoLevels.GetEnumerator();
+                enumerator.MoveNext();
+                foreach (var kvp in infoLevels)
+                {
+                    enumerator.MoveNext();
+                    if (kvp.Value == _infoLevel.Level && _infoLevel.MaximumLenghtProgressBar < _infoLevel.CountLetters)
+                    {
+                        _infoLevel.MaximumLenghtProgressBar = enumerator.Current.Key;
+                        _infoLevel.Level = enumerator.Current.Value;
+                        _infoLevel.CountLetters = 0;
+                        break;
+                    }
+                }
+            }
+            UpdateProgressBar(progressBar, ref textBox);
+            Serialize();
+            return progressBar;
+        }
+
+        private ProgressBar UpdateProgressBar(ProgressBar progressBar, ref TextBlock textBox)
+        {
+            progressBar.Maximum = _infoLevel.MaximumLenghtProgressBar;
+            progressBar.Value = _infoLevel.CountLetters;
+            textBox.Text = $"Your level: {_infoLevel.Level.ToString()}";
+            return progressBar;
+        }
+    }
+    [Serializable]
+    public class InfoLevel
+    {
+        public NameLevel Level { get; set; }
+        public int CountLetters { get; set; }
+        public int MaximumLenghtProgressBar
+        {
+            get; set;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TrainerTextsWPF;
+
+namespace BlindTyping
+{
+    public partial class MainWindow : Window
+    {
+        public static DataContent data = new DataContent();
+        private GraphLevel graphLevel = new GraphLevel();
+        private FileManager fileManager;
+        private int ErrorCount;
+        private bool _isTrainingStart = false;
+        Stopwatch stopwatch = new Stopwatch();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ProgressBarValue = graphLevel.UpdateGraph(ProgressBarValue, ref Namelevel);
+            fileManager = new FileManager();
+            fileManager.GetFilesInPath();
+            comboBoxFiles = fileManager.SetValueComboBox(comboBoxFiles);
+
+            data = fileManager.Deserialize(fileManager.GetFirstFile());
+            ContentText.Text = data.Text;
+        }
+
+        private void СreateFile(object sender, RoutedEventArgs e)
+        {
+            Window window = new Window()
+            {
+                Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#dad7cd")),
+                Height = 200,
+                Width = 200
+            };
+            StackPanel panel = new StackPanel()
+            {
+                Orientation = Orientation.Vertical,
+            };
+            Label label = new Label()
+            {
+                Content = "Название файла"
+            };
+            TextBox textBox = new TextBox();
+            panel.Children.Add(label);
+            panel.Children.Add(textBox);
+            window.Content = panel;
+            window.ShowDialog();
+            if (textBox.Text == "" || File.Exists(System.IO.Path.Combine(fileManager.GetPathFile(), $"{textBox.Text}.json")))
+            {
+                MessageBox.Show("Файл либо существует, либо поле пустое.");
+                return;
+            }
+            else
+            {
+                DataContent dataContent = new DataContent();
+                dataContent.NameFile = textBox.Text;
+                dataContent.Text = "";
+
+                using (File.Create(System.IO.Path.Combine(fileManager.GetPathFile(), $"{textBox.Text}.json"))) { };
+                fileManager.Serialize(dataContent, dataContent.NameFile);
+                fileManager.AddFile(dataContent.NameFile);
+
+                comboBoxFiles = fileManager.SetValueComboBox(comboBoxFiles);
+
+            }
+
+        }
+
+        private void EditText(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox1 = new TextBox()
+            {
+                VerticalScrollBarVisibility = ScrollBarVisibility.Visible,
+                FontSize = 15,
+                Text = ContentText.Text,
+                TextWrapping = TextWrapping.Wrap,
+                BorderThickness = new Thickness(0)
+            };
+            Window window = new Window()
+            {
+                Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#dad7cd"))
+            };
+
+            window.Content = textBox1;
+            window.ShowDialog();
+            data.Text = textBox1.Text;
+            fileManager.Serialize(data, data.NameFile);
+
+            ContentText.Text = textBox1.Text;
+            TextBoxInput.Text = "";
+
+            stopwatch.Stop();
+            _isTrainingStart = false;
+        }
+
+        private void DeleteText(object sender, RoutedEventArgs e)
+        {
+            TextBoxInput.Text = "";
+            stopwatch.Reset();
+            _isTrainingStart = false;
+        }
+
+        private void isErrorIngored_Click(object sender, RoutedEventArgs e)
+        {
+            if (isErrorIngored.IsChecked ?? false)
+            {
+                if (TextBoxInput.Text.Length == 0)
+                    return;
+                if (MessageBox.Show("введенный вами текст - будет удален. Точно применить?", "Внимание", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+                {
+                    TextBoxInput.Text = "";
+                }
+
+            }
+        }
+
+        private void isCommasIngored_Click(object sender, RoutedEventArgs e)
+        {
+            if (isCommasIngored.IsChecked ?? false)
+            {
+                string tempText = ContentText.Text;
+                tempText = Regex.Replace(tempText, @"\W+", " ");
+                ContentText.Text = tempText;
+                stopwatch.Reset();
+                TypingTime.Text = "";
+                _isTrainingStart = false;
+            }
+        }
+
+        void OnComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            int selectedIndex = comboBox.Items.IndexOf(comboBox.SelectedItem);
+
+            data = fileManager.Deserialize(fileManager.GetIndexFile(selectedIndex));
+            ContentText.Text = data.Text;
+        }
+        private async void ShowTime()
+        {
+            while (true)
+            {
+                await Task.Delay(500);
+                Dispatcher.Invoke(() =>
+                {
+                    if (stopwatch.IsRunning)
+                        TypingTime.Text = stopwatch.Elapsed.TotalSeconds.ToString("0.000");
+                    else
+                        return;
+                });
+            }
+        }
+        private void TextBoxInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (ContentText.Text.Length == 0)
+                {
+                    string tempText = TextBoxInput.Text;
+                    string substring = tempText.Substring(0, tempText.Length - 1);
+                    TextBoxInput.Text = substring;
+                    ErrorCount = 0;
+                    return;
+                }
+
+                if (!_isTrainingStart && TextBoxInput.Text.Length != 0)
+                {
+                    stopwatch.Restart();
+                    _isTrainingStart = true;
+                    ShowTime();
+                }
+                if (TextBoxInput.Text.Length == ContentText.Text.Length)
+                {
+                    stopwatch.Stop();
+                    ResultGeneral(stopwatch.Elapsed.TotalSeconds, ErrorCount, ContentText.Text.Length);
+                    ProgressBarValue = graphLevel.UpdateGraph(ProgressBarValue, ref Namelevel, ContentText.Text.Length);
+                    ErrorCount = 0;
+                    _isTrainingStart = false;
+                    TextBoxInput.Text = "";
+                }
+                else
+                {
+                    string textFromTxt = ContentText.Text;
+                    string textInput = TextBoxInput.Text;
+
+                    for (int i = 0; i < textInput.Length; i++)
+                    {
+                        if (textInput[i] != textFromTxt[i])
+                        {
+                            if (isErrorIngored.IsChecked ?? false)
+                            {
+                                string tempText = TextBoxInput.Text;
+                                string substring = tempText.Substring(0, tempText.Length - 1);
+                                TextBoxInput.Text = substring;
+                                TextBoxInput.CaretIndex = substring.Length;
+                            }
+                            else
+                            {
+                                ErrorCount++;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                MessageBox.Show($"{"Добавьте текст для печати."}");
+                return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}");
+                return;
+            }
+        }
+        
+        private void ResultGeneral(double time, int errors, int length)
+        {
+
+            double resultRPM = length / time;
+            TypingSpeed.Text = $"Длина текста: {length}";
+            CountErrors.Text = $"Кол-во ошибок: {errors}";
+            LengthText.Text = $"Скорость печати: {resultRPM:F3} RPM";
+        }
+
+    }
+    public class FileManager
+    {
+        private List<string> files = new List<string>();
+        private static string PathDirectory = $"{System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "TrainerTexts")}";
+        private string PathFile = $"{System.IO.Path.Combine(PathDirectory, "Texts")}";
+        public string GetIndexFile(int index) =>  files[index];
+        public string GetPathFile() => PathFile;
+        public void AddFile(string nameFile) => files.Add(nameFile);
+        public string GetFirstFile() => files[0];
+
+        public FileManager()
+        {
+            CheakExtension();
+        }
+        public ComboBox SetValueComboBox(ComboBox comboBox)
+        {
+            comboBox.ItemsSource = null;
+            comboBox.ItemsSource = files;
+            return comboBox;
+        }
+        public void GetFilesInPath()
+        {
+            try
+            {
+                DirectoryInfo? dir = new DirectoryInfo(PathFile);
+                FileInfo[] fileList = dir.GetFiles();
+
+                foreach (var file in fileList)
+                {
+                    files.Add(System.IO.Path.GetFileNameWithoutExtension(file.FullName));
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"{e}");
+            }
+        }
+        public void Serialize(DataContent dataContent, string nameFile)
+        {
+
+            string text = JsonSerializer.Serialize(dataContent);
+
+            using StreamWriter writer = new StreamWriter(System.IO.Path.Combine(PathFile, $"{nameFile}.json"));
+            writer.Write(text);
+            writer.Close();
+        }
+        public DataContent Deserialize(string fileName)
+        {
+            string pathFile = System.IO.Path.Combine(PathFile, $"{fileName}.json");
+            using StreamReader reader = new StreamReader(pathFile);
+            string json = reader.ReadToEnd();
+
+            DataContent? dataContent = JsonSerializer.Deserialize<DataContent>(json);
+
+            if (dataContent is not null)
+                return dataContent;
+            else
+                throw new ArgumentNullException($"Invalid deserialize {pathFile}");
+        }
+        public void CheakExtension()
+        {
+
+            if (!Directory.Exists(PathDirectory))
+                Directory.CreateDirectory(PathDirectory);
+
+            if (!Directory.Exists(PathFile))
+            {
+                Directory.CreateDirectory(PathFile);
+                DataContent dataContent = new DataContent()
+                {
+                    NameFile = "Русский набор",
+                    Text = "Cтолица России - Москва"
+                };
+                using (File.Create(System.IO.Path.Combine(PathFile, $"{dataContent.NameFile}.json"))) { };
+                Serialize(dataContent, dataContent.NameFile);
+                MainWindow.data = dataContent; // передаю как первый файл  
+            }
+        }
+    }
+    [Serializable]
+    public class DataContent
+    {
+        public string? Text { get; set; }
+        public string? NameFile { get; set; }
+    }
+
+}
